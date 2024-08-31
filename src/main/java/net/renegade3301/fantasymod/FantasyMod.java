512cdd7327144dc50d642758edf9e024fac75de5
Renegade3301 <hdea3301@gmail.com>
2024-08-31 15:47:13 -0500

    c

--- src/main/java/net/renegade3301/fantasymod/FantasyMod.java
+++ src/main/java/net/renegade3301/fantasymod/FantasyMod.java
@@ -11,6 +11,5 @@ public class FantasyMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
 	}
 }
--- src/main/java/net/renegade3301/fantasymod/item/ModItems.java
+++ src/main/java/net/renegade3301/fantasymod/item/ModItems.java
@@ -1,4 +1,26 @@
 package net.renegade3301.fantasymod.item;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.renegade3301.fantasymod.FantasyMod;
+
 public class ModItems {
+    public static final Item REFINED_EMERALD = registerItem("refined_emerald", new Item(new Item.Settings()));
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(FantasyMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        FantasyMod.LOGGER.info("Registering Mod Items for " + FantasyMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(REFINED_EMERALD);
+        });
+    }
 }
